e16c52694235cb9d584cfc7200231c0c14774943
lolisa <1292652198@qq.com>
2014-12-14 19:03:24 +0800

    Fix compiler error.

--- can_call.hpp
+++ can_call.hpp
@@ -3,14 +3,14 @@
 #include <boost/mpl/bool.hpp>
 namespace misc
 {
-	template< typename T, typename ... R >
-	struct can_call
-	{
-		template< typename t >
-		static constexpr boost::mpl::true_ SFINAE( t, typename boost::remove_reference< decltype( std::declval< t >( )( std::declval< R >( )... ) ) >::type * = nullptr )
-		{ return boost::mpl::true_( ); }
-		static constexpr boost::mpl::false_ SFINAE( ... ) { return boost::mpl::false_( ); }
-		static constexpr bool value = decltype( SFINAE( std::declval< T >( ) ) )::value;
-	};
+    template< typename T, typename ... R >
+    struct can_call
+    {
+        template< typename t >
+        static constexpr boost::mpl::true_ SFINAE( t, typename std::remove_reference< decltype( std::declval< t >( )( std::declval< R >( )... ) ) >::type * = nullptr )
+        { return boost::mpl::true_( ); }
+        static constexpr boost::mpl::false_ SFINAE( ... ) { return boost::mpl::false_( ); }
+        static constexpr bool value = decltype( SFINAE( std::declval< T >( ) ) )::value;
+    };
 }
 #endif // CAN_CALL_HPP
